33a487f68628f03260146308037aae57aaeba0bd
Dimash <nogaybaevd11@list.ru>
2023-09-07 20:56:59 +0600

    Initial commit

---
+++ CRUDTelephony/src/test/java/com/CRUDTelephony/CRUDTelephony/CrudTelephonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUDTelephony.CRUDTelephony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTelephonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
