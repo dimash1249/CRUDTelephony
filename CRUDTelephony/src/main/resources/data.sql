dfff9886bd1e51772284be344096019bf6976955
Dimash <nogaybaevd11@list.ru>
2023-09-10 21:46:23 +0600

    Add data database

---
+++ CRUDTelephony/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO person_phones (person_id, person_name, person_birth_year, person_phone_first, person_phone_second, person_creation_date)
+VALUES (2, 'Dimash', 2001, '+77473334215', '+77473334215', NOW());
+INSERT INTO person_phones (person_id, person_name, person_birth_year, person_phone_first, person_phone_second, person_creation_date)
+VALUES (3, 'Dimash', 2001, '+77473334215', '+77473334215', NOW());
